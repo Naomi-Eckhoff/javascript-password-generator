ef8b6b803014dca34dcc3c3b040469c38f40177a
Naomi Eckhoff <nsuibhne@gmail.com>
2021-10-09 20:34:38 -0500

    built the character selection loop and array

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,14 +16,11 @@ function writePassword() {
 //add function for generation of password and return 
 function generatePassword() {
   //add prompt with var dec length (8 to 128)?
-  var passLength = 0;
+  var passLength;
 
-  // Is it weird? Yes. Does it handle data validation and resetting the variable on pressing the button again? Also yes. EFFICIENCY!!!!!! *lies I tell myself*
-  while (passLength < 8 || passLength > 128) {
-    passLength = window.prompt("How long should the password be (8 to 128 required)");
-    if (passLength != null) {
-      passLength = parseInt(passLength);
-    }
+  // Is it weird? Yes. Does it handle data validation? Also yes. EFFICIENCY!!!!!! *lies I tell myself*
+  while (isNaN(passLength) || passLength < 8 || passLength > 128) {
+    passLength = parseInt(window.prompt("How long should the password be (8 to 128 required)"));
   }
 
   //add prompt with var dev and if/switch include lowercase?
@@ -38,7 +35,7 @@ function generatePassword() {
   var charactersArray = [];
 
   while (charactersArray.length === 0) {
-    includedCharacters = window.prompt("What characters should be used? Enter you choices as digits all at once. Format is irrelevant. 1 Lowercase. 2 Capitals. 3 Numbers. 4 Special Characters.")
+    includedCharacters = window.prompt("What characters should be used? Enter you choices as digits all at once. Format is irrelevant. 1 Lowercase. 2 Capitals. 3 Numbers. 4 Special Characters. At least 1 number is required.")
 
     if (includedCharacters != null) {
       storageArray = includedCharacters.split("");
@@ -61,20 +58,59 @@ function generatePassword() {
     }
   }
   //Yep, that'll validate it
+  //Due to some issues over on the parseInt I error checked this and it seems bullet proof
 
 
   //add prompt with var dec with switch no breaks. Those are for cowards. Only switch looping!
   var alphabet = "abcdefghijklmnopqrstuvwxyz";
   var ALPHABET = alphabet.toUpperCase(); // It's a dumb joke, but I refuse to not use it.
   var numerals = "0123456789";
-  var specialChars = "\"'!#$%&()*+,-./:;<=>?@[]^_`{|}~";
+  var specialChars = "\\\"'!#$%&()*+,-./:;<=>?@[]^_`{|}~";
+  var selectionArray = [];
+  var selectionArrayLength = 0;
 
-  var specialCharsArray = specialChars.split("");
-  //insert other function maybe
 
-  //trigger password update if needed
+
+  //I got to learn that javascript does multidimensional arrays. I'm impressed but also slightly annoyed so now this exists.
+  charactersArray.forEach(element => {
+    switch (element) {
+      case 1:
+        alphabet = alphabet.split("");
+        alphabet.forEach(element => {
+          selectionArray.push(element);
+        });
+        selectionArrayLength += alphabet.length;
+        break;
+      case 2:
+        ALPHABET = ALPHABET.split("");
+        ALPHABET.forEach(element => {
+          selectionArray.push(element);
+        });
+        selectionArrayLength += ALPHABET.length;
+        break;
+      case 3:
+        numerals = numerals.split("");
+        numerals.forEach(element => {
+          selectionArray.push(element);
+        });
+        selectionArrayLength += numerals.length;
+        break;
+      case 4:
+        specialChars = specialChars.split("");
+        specialChars.forEach(element => {
+          selectionArray.push(element);
+        });
+        selectionArrayLength += specialChars.length;
+        break;
+    }
+  });
 
 }
+//insert other function maybe
+
+//trigger password update if needed
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
